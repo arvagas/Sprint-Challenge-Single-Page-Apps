bc2254487c6c4d4389870f9dc007f3a0b002ff50
Arvin Agas <arvagas@gmail.com>
2019-07-26 12:06:31 -0400

    created some new styles

---
+++ src/styles/StylesCard.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const Card = styled.div`
+    display: flex;
+    flex-direction: column;
+    border: solid lightgray 1px;
+    border-radius: 15px;
+    width: 400px;
+    margin: 2rem;
+`
+
+export default Card
