81cfabe389c320fcbd060380f2e7ca8d016b10da
Arvin Agas <arvagas@gmail.com>
2019-07-26 12:30:06 -0400

    utilized semantic-ui over styled-components

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,23 +1,27 @@
 import React from 'react'
-
-import Card from '../styles/StylesCard'
-import Image from '../styles/StylesImage'
-import Name from '../styles/StylesName'
-import Location from '../styles/StylesLocation'
-import MinCont from '../styles/StylesMinCont'
+import { Card, Icon, Image } from 'semantic-ui-react'
 
 export default function CharacterCard ( { char } ) {
   return (
     <Card>
-      <Image src={char.image}></Image>
-      <MinCont>
-        <Name>{char.name}</Name>
-        <p>{char.species} {char.status}</p>
-      </MinCont>
-      <MinCont>
-        <Location>Location: {char.location.name}</Location>
-        <Location>Origin: {char.origin.name}</Location>
-      </MinCont>
+      <Image src={char.image} wrapped ui={false} />
+
+      <Card.Content>
+        <Card.Header>{char.name}</Card.Header>
+        <Card.Meta>{char.species} {char.status}</Card.Meta>
+        <Card.Description>
+          Location: {char.location.name}
+          <br></br>
+          Origin: {char.origin.name}
+        </Card.Description>
+      </Card.Content>
+
+      <Card.Content extra>
+        <a>
+          <Icon name='user' />
+          {char.episode.length} Episodes
+        </a>
+      </Card.Content>
     </Card>
   )
 }
