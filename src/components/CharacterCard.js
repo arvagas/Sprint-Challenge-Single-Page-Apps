4f5c5ddb9dcca0a4585f7a2c2831d5cf6a3ff17c
Arvin Agas <arvagas@gmail.com>
2019-07-26 12:07:08 -0400

    Character page complete with imported styles

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,23 @@
 import React from 'react'
 
-export default function CharacterCard ({ /* add props */ }) {
-  return (<span>todo: location</span>
+import Card from '../styles/StylesCard'
+import Image from '../styles/StylesImage'
+import Name from '../styles/StylesName'
+import Location from '../styles/StylesLocation'
+import MinCont from '../styles/StylesMinCont'
+
+export default function CharacterCard ( { char } ) {
+  return (
+    <Card>
+      <Image src={char.image}></Image>
+      <MinCont>
+        <Name>{char.name}</Name>
+        <p>{char.species} {char.status}</p>
+      </MinCont>
+      <MinCont>
+        <Location>Location: {char.location.name}</Location>
+        <Location>Origin: {char.origin.name}</Location>
+      </MinCont>
+    </Card>
+  )
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,34 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+import CharacterCard from './CharacterCard'
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+  // API website values
+  const apiSite = 'https://rickandmortyapi.com/api'
+  const apiCategory = 'character'
+
+  const [characters, setCharacters] = useState([])
 
   useEffect(() => {
-    // TODO: Add AJAX/API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios.get(`${apiSite}/${apiCategory}/`)
+    .then(res => {
+      console.log('Data retrieved: ', res)
+      const apiChars = res.data.results
+
+      setCharacters(apiChars)
+    })
+
+    .catch(err => {
+      console.log('API error: ', err)
+    })
   }, [])
 
-  return <section className='character-list grid-view'>
-
-      <h2>TODO: `array.map()` over your state here!</h2>
+  return (
+    <section className='character-list grid-view'>
+      {characters.map(char => (
+        <CharacterCard key={char.name} char={char}/>
+      ))}
     </section>
-
+  )
 }
